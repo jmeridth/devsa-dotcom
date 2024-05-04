8509a0b12fcad8434a900c2e227363b794c989c0
ckeller22 <chris.keller.dev@gmail.com>
2024-05-04 12:39:57 -0500

    style: add some padding for scrollbar so border not as harsh

--- app/components/CTA/Calendar.jsx
+++ app/components/CTA/Calendar.jsx
@@ -15,12 +15,11 @@ export function CalendarSection() {
          </SectionIntro>
          <Container className="mt-16">
             <FadeIn>
-               <div className="">
+               <div className="rounded-2xl bg-[#212325] py-2 pr-2">
                   <iframe
                      src="https://lu.ma/embed/calendar/cal-Jg0KUhNjt4lLR55/events?compact=true"
                      style={{
-                        border: '1px solid #bfcbda88',
-                        borderRadius: '24px',
+                        borderRadius: '12px',
                         width: '100%',
                         height: '600px',
                      }}
